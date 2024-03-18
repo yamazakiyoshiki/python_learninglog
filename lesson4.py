7a45eb27746e89ae31159650d7f5e03b6582639a
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-03-18 22:05:20 +0900

    lesson4 learning

--- lesson4.py
+++ lesson4.py
@@ -94,3 +94,179 @@ def outer(a, b):
     print(r)
 
 outer(1, 2)
+
+def outer(a, b):
+
+    def inner():
+        return a + b
+
+    return inner
+
+f = outer(1, 2)
+r = f()
+print(r)
+
+def circle_area_func(pi):
+    def circle_area(radius):
+        return pi * radius * radius
+
+    return circle_area
+
+cal1 = circle_area_func(3.14)
+cal2 = circle_area_func(3.141592)
+
+print(cal1(10))
+print(cal2(10))
+
+def print_more(func):
+    def wrapper(*args, **kwargs):
+        print('func:', func.__name__)
+        print('args:', args)
+        print('kwargs:', kwargs)
+        result = func(*args, **kwargs)
+        print('result:', result)
+        return result
+    return wrapper
+
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+    return wrapper
+
+@print_info
+@print_more
+def add_num(a, b):
+    return a + b
+
+r = add_num(10, 20)
+print(r)
+
+@print_info
+def sub_num(a, b):
+    return a - b
+# print('start')
+# r = add_num(10, 20)
+# print('end')
+
+# print(r)
+
+# f = print_info(add_num)
+# r = f(10, 20)
+# print(r)
+
+l = ['Mon', 'tue', 'Wed', 'Thu', 'fri', 'sat', 'Sun']
+
+def change_words(words, func):
+    for word in words:
+        print(func(word))
+
+def sample_func(word):
+    return word.capitalize()
+
+def sample_func2(word):
+    return word.lower()
+# sample_func = lambda word: word.capitalize()
+
+change_words(l, lambda word: word.capitalize())
+
+change_words(l, lambda word: word.lower())
+
+l = ['Good morning', 'Good afternoon', 'Good night']
+
+for i in l:
+    print(i)
+
+print("##############")
+
+def greeting():
+    yield 'Good morning'
+    yield 'Good afternoon'
+    yield 'Good night'
+
+# for g in greeting():
+#     print(g)
+
+g = greeting()
+print(next(g))
+print("@@@@@")
+print(next(g))
+print("@@@@@")
+print(next(g))
+
+def counter(num=10):
+    for _ in range(num):
+        yield 'run'
+
+c = counter()
+
+print(next(c))
+print("@@@@@@")
+print(next(c))
+print("@@@@@@")
+print(next(c))
+print("@@@@@@")
+print(next(c))
+print("@@@@@@")
+print(next(c))
+
+
+t = (1, 2, 3, 4, 5)
+t2 = (5, 6, 7, 8, 9, 10)
+
+r = []
+for i in t:
+    r.append(i)
+
+print(r)
+
+r = [i for i in t if i % 2 ==0]
+
+print(r)
+
+r = []
+for i in t:
+    for j in t2:
+        r.append(i * j)
+
+print(r)
+
+r = [i * j for i in t for j in t2]
+
+print(r)
+
+w = ['mon', 'tue', 'wed']
+f = ['coffee', 'milk', 'water']
+d = {}
+for x, y in zip(w, f):
+    d[x] = y
+
+print(d)
+
+d = {x: y for x, y in zip(w, f)}
+print(d)
+
+s = set()
+
+for i in range(10):
+    if i % 2 == 0:
+        s.add(i)
+
+print(s)
+
+s = {i for i in range(10) if i % 2 == 0}
+print(s)
+
+def g():
+    for i in range(10):
+        yield i
+
+g = g()
+print(type(g))
+
+g = (i for i in range(10) if i % 2 == 0)
+
+for x in g:
+    print(x)
